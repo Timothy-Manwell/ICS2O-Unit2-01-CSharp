640e167aee139e199c333c73214bb310d73079c8
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-05 18:16:55 +0000

    changed c#

--- main.cs
+++ main.cs
@@ -5,8 +5,10 @@
 
 using System;
 
-class Program {
-    public static void Main (string[] args) {
+class Program 
+{
+    public static void Main (string[] args) 
+    {
     Console.WriteLine ("Hello World");
     
     Console.WriteLine ("\nDone");
