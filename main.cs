53d74398ebc80979279cca00f12be019037b64cc
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-05 18:09:55 +0000

    changed c#

--- main.cs
+++ main.cs
@@ -1,13 +1,14 @@
 //Created by: Timothy Manwell
 //Created on: Oct 5
 //
-//This program diplays "Hello, World!"
+//This program displays "Hello, World!"
 
 using System;
 
 class Program {
-  public static void Main (string[] args) {
+    public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+    
     Console.WriteLine ("\nDone");
-  }
+    }
 }
